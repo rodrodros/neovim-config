1eb0b317b01fb6c50e25ccb18566e708ac27110e
Rodrigo <rodrodpino@gmail.com>
2025-09-23 10:05:52 +0100

    chore: [wip] support for navic

--- lua/plugins/bars.lua
+++ lua/plugins/bars.lua
@@ -1,7 +1,14 @@
 return {
 	{
 		"nvim-lualine/lualine.nvim",
-		dependencies = { { "nvim-tree/nvim-web-devicons", opt = true } },
+		dependencies = {
+			{ "nvim-tree/nvim-web-devicons", opt = true },
+		},
+		ops = function(_, opts)
+			if not vim.g.trouble_lualine then
+				table.insert(opts.sections.lualine_c, { "navic", color_correction = "dynamic" })
+			end
+		end,
 		config = function()
 			require("lualine").setup({
 				options = {
@@ -63,4 +70,30 @@ return {
 			vim.keymap.set("n", "π", "<Cmd>BufferLineMovePrev<CR>", { noremap = true, silent = true })
 		end,
 	},
+	-- Needs configuration with the LSP
+	-- {
+	-- 	"SmiteshP/nvim-navic",
+	-- 	dependencies = {
+	-- 		"nvim-lspconfig",
+	-- 	},
+	-- 	lazy = true,
+	-- 	init = function()
+	-- 		-- vim.g.navic_silenece = true
+	-- 		local lazyvim = require("lazyvim.util")
+	-- 		lazyvim.lsp.on_attach(function(client, buffer)
+	-- 			if client.supports_method("textDocument/documentSymbol") then
+	-- 				require("nvim-navic").attach(client, buffer)
+	-- 			end
+	-- 		end)
+	-- 	end,
+	-- 	opts = function()
+	-- 		return {
+	-- 			separator = " ",
+	-- 			highligh = true,
+	-- 			depth_limit = 5,
+	-- 			-- icons = LazyVim.config.icons.kinds,
+	-- 			lazy_update_context = true,
+	-- 		}
+	-- 	end,
+	-- },
 }
